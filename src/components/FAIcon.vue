b84495bdf89ff741300a2753804b8b2d0e400fff
WithoutAName25 <72936889+WithoutAName25@users.noreply.github.com>
2021-11-02 20:13:39 +0100

    Added icon to navigation

---
+++ src/components/FAIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  type: {
+    default: "fas",
+    type: String,
+    validator(value: unknown): boolean {
+      return value == "fas" || value == "fab" || value == "fa-solid" || value == "fa-brands";
+    }
+  },
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+<i :class="[type, 'fa-' + name]"/>
+</template>
--- src/components/navigation/NavItems.vue
+++ src/components/navigation/NavItems.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import NavItem from "./NavItem.vue"
+import FAIcon from "../FAIcon.vue";
 </script>
 
 <template>
   <div class="items">
-    <NavItem href="/">Home</NavItem>
+    <NavItem href="/"><FAIcon name="home"/> Home</NavItem>
     <NavItem href="/maven/" external>Maven</NavItem>
   </div>
 </template>
