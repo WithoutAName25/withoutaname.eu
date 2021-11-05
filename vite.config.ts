a1c14267df4ae8adec12ff3534aef15fb5dd7bb7
WithoutAName25 <72936889+WithoutAName25@users.noreply.github.com>
2021-11-05 17:03:22 +0100

    Added polyfill.io

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import legacy from '@vitejs/plugin-legacy'
 import { dependencies } from './package.json';
 
 const manualVendorChunks: {[k: string]: string[]} = {
@@ -29,5 +30,8 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()]
+  plugins: [
+      vue(),
+      // legacy()
+  ]
 })
