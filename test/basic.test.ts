c73b7b170df887e988df760ec35cd8678b789b13
WithoutAName25 <withoutaname@withoutaname.eu>
2022-03-20 17:34:44 +0100

    Testing

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,10 +1,7 @@
 import { describe, expect, it } from "vitest"
-import { FlipFlop } from "~/scripts/tools/digital-electronics/flip-flop"
-import { FlipFlopSettings } from "~/scripts/tools/digital-electronics/flip-flop-settings"
 
 describe("tests", () => {
   it("should works", () => {
-    new FlipFlop(new FlipFlopSettings())
     expect(1 + 1).toEqual(2)
   })
 })
