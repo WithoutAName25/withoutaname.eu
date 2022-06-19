cdc21e87c9801eaeb2f4187093dbde5e3188609b
WithoutAName25 <withoutaname@withoutaname.eu>
2022-06-20 00:16:10 +0200

    Enable prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-vercel"
 import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,6 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			default: true,
+		},
 	},
 }
 
